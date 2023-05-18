75d7fffe9387937a584b216f413583888d224112
Milord-ThatOneModder <100714051+Milord-ThatOneModder@users.noreply.github.com>
2023-05-18 17:20:13 +0200

    fixed file endings

--- Lua/Autorun/luaDisplaySideBySide.lua
+++ Lua/Autorun/luaDisplaySideBySide.lua
@@ -1,4 +1,41 @@
-local findtarget = dofile(... .. "/Lua/findtarget.lua")
+local function FindClosestItem(submarine, position)
+    local closest = nil
+    for key, value in pairs(submarine and submarine.GetItems(false) or Item.ItemList) do
+        if value.Linkable and not value.HasTag("notlualinkable") and not value.HasTag("crate") and not value.HasTag("ammobox") and not value.HasTag("door") and not value.HasTag("smgammo") and not value.HasTag("hmgammo") then
+            -- check if placabke or if it does not have holdable component
+            local check_if_p_or_nh = false
+            local holdable = value.GetComponentString("Holdable")
+            if holdable == nil then
+                check_if_p_or_nh = true
+            else
+                if holdable.attachable == true then
+                    check_if_p_or_nh = true
+                end
+            end
+            if check_if_p_or_nh == true then
+                if Vector2.Distance(position, value.WorldPosition) < 100 then
+                    if closest == nil then closest = value end
+                    if Vector2.Distance(position, value.WorldPosition) <
+                        Vector2.Distance(position, closest.WorldPosition) then
+                        -- this should prevent items that are inside invetories be linkable
+                        if value.ParentInventory == nil then
+                            closest = value
+                        end
+                    end
+                end
+            end
+        end
+    end
+    return closest
+end
+
+local function FindClientCharacter(character)
+    if CLIENT then return nil end
+
+    for key, value in pairs(Client.ClientList) do
+        if value.Character == character then return value end
+    end
+end
 
 local function AddMessage(text, client)
     local message = ChatMessage.Create("Lua Linker", text, ChatMessageType.Default, nil, nil)
@@ -13,12 +50,17 @@ end
 
 local linksDisplaySideBySide = {}
 
-Hook.Add("luaDisplaySideBySide.onUse", "lualinker.luaDisplaySideBySide",function(statusEffect, delta, item)
-    local target = findtarget.findtarget(item)
-    if CLIENT and Game.IsMultiplayer then 
+Hook.Add("luaDisplaySideBySide.onUse", "examples.luaDisplaySideBySide",
+         function(statusEffect, delta, item)
+    if CLIENT and Game.IsMultiplayer then return end -- server side only in multiplayer, client side in singleplayer
+
+    if item.ParentInventory == nil or item.ParentInventory.Owner == nil then
         return
     end
-    local owner = findtarget.FindClientCharacter(item.ParentInventory.Owner)
+
+    local owner = FindClientCharacter(item.ParentInventory.Owner)
+
+    local target = FindClosestItem(item.Submarine, (item.ParentInventory.Owner).CursorWorldPosition)
 
     if target == nil then
         AddMessage("No item found", owner)
